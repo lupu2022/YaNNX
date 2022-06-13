58d0a6efbe818eae690f40deeae86f94f07df014
Zhou Chang <achang.zhou@gmail.com>
2022-06-13 14:44:47 +0800

    clean code

--- dnnl/layernorm.hpp
+++ dnnl/layernorm.hpp
@@ -2,7 +2,9 @@ template <tt::TensorDataType _DTYPE_>
 tt::OperatorReturnType DNNLTensor<_DTYPE_>::onnx_LayerNormalization(tt::tensor_t X, tt::tensor_t Scale, std::variant<void *, tt::tensor_t>& B, tt::tensor_t Y, std::variant<void *, tt::tensor_t>& Mean, std::variant<void *, tt::tensor_t>& InvStdDev, int64_t axis, float epsilon, int64_t stash_type) {
     tt::TensorDataType dtype = _DTYPE_;
 
-    yannx_assert( (size_t)axis == (X->shape().size() - 1), "DNNL's layernormliation only support last dimention as channel");
+    if ( axis != - 1) {
+        yannx_assert( (size_t)axis == (X->shape().size() - 1), "DNNL's layernormliation only support last dimention as channel");
+    }
     yannx_assert( B.index() == 1, "DNNL's layernormliation must include B");
     yannx_assert( (Mean.index() == 0) && (InvStdDev.index() == 0), "DNN's layernormliation don't support other output");
 
@@ -13,8 +15,8 @@ tt::OperatorReturnType DNNLTensor<_DTYPE_>::onnx_LayerNormalization(tt::tensor_t
         auto shift_mem = dnnl( std::get<1>(B))->dnnl_mem();
 
         dnnl_layer_normalization_desc_t desc;
-        DNNL_CHECK(dnnl_layer_normalization_forward_desc_init(&desc, dnnl_forward_training, src_md, nullptr, epsilon,
-                                                            dnnl_use_scale | dnnl_use_shift  ));
+        DNNL_CHECK(dnnl_layer_normalization_forward_desc_init(&desc, dnnl_forward_inference, src_md, nullptr, epsilon,
+                                                            dnnl_use_scale | dnnl_use_shift | dnnl_normalization_flags_none   ));
 
         dnnl_primitive_desc_t ln_pd;
         DNNL_CHECK(dnnl_primitive_desc_create(&ln_pd, &desc, nullptr, dnnl_help::DNNL_ENGINE_DEFAULT, nullptr));
@@ -24,7 +26,6 @@ tt::OperatorReturnType DNNLTensor<_DTYPE_>::onnx_LayerNormalization(tt::tensor_t
         // create destnation memory
         dnnl(Y)->reorder(dnnl_primitive_desc_query_md(ln_pd, dnnl_query_dst_md, 0));
         auto dst_mem = dnnl(Y)->dnnl_mem();
-
 
         // prepare arguments and execute
         dnnl_exec_arg_t args[4];
